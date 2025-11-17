84bc96bb3c4b0bda3a60771d9867412f6c093d90
cckkuma <98962929+cckkuma@users.noreply.github.com>
2025-11-17 16:16:10 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -1,7 +1,7 @@
 -- ============================================
 -- 1. Systems（系統表）
 -- ============================================
-CREATE TABLE department
-    ( dept_id    INTEGER NOT NULL PRIMARY KEY
+CREATE TABLE systems( 
+    dept_id    INTEGER NOT NULL PRIMARY KEY
     , dept_name  VARCHAR(50) NOT NULL
     ) ;
