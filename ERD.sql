b1777ab4e8d707ae2e282de95b3920ed3c328747
cckkuma <98962929+cckkuma@users.noreply.github.com>
2025-11-17 16:13:25 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -5,7 +5,7 @@
 CREATE TABLE systems (
     system_id SERIAL PRIMARY KEY,
     system_code VARCHAR(50) NOT NULL UNIQUE,
-    system_name VARCHAR(200) NOT NULL
+    system_name VARCHAR(200) 
 );
 
 -- 註釋
