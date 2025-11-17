983c339e3b7907108bd30ceb03b5773c8ef70506
cckkuma <98962929+cckkuma@users.noreply.github.com>
2025-11-17 16:17:36 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -3,5 +3,5 @@
 -- ============================================
 CREATE TABLE train_systems( 
     dept_id    INTEGER NOT NULL PRIMARY KEY
-    , dept_name  VARCHAR(50) NOT NULL
+    , dept_name  INTEGER NOT NULL
     ) ;
