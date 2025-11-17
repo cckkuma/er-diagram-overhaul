82271df4b0028c7751e49c42b30696478bbf0b3f
cckkuma <98962929+cckkuma@users.noreply.github.com>
2025-11-17 16:04:43 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -5,7 +5,7 @@ CREATE TABLE systems (
     system_id SERIAL PRIMARY KEY,
     system_code VARCHAR(50) NOT NULL UNIQUE,
     system_name VARCHAR(200) NOT NULL,
-);
+    );
 
 -- 註釋
 COMMENT ON TABLE systems IS '列車系統表';
