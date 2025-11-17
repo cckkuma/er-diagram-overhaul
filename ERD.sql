024845db6445bbb023c59b83f477b6854d187104
cckkuma <98962929+cckkuma@users.noreply.github.com>
2025-11-17 16:21:52 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -2,6 +2,6 @@
 -- 1. Systems（系統表）
 -- ============================================
 CREATE TABLE train_systems( 
-    dept_id    INTEGER NOT NULL PRIMARY KEY, 
-    dept_name  INTEGER NOT NULL
+    dept_id    INTEGER NOT NULL PRIMARY KEY 
+    , dept_name  INTEGER NOT NULL
     ) ;
